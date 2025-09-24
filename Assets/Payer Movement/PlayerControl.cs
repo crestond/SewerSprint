7c78d060e8eb76c200f78f1fa7ee3d89dafb6e8c
crestondorothy <143129042+crestondorothy@users.noreply.github.com>
2025-09-24 01:13:26 -0500

    Working Version of SewerSprint_v0.0.1

--- Assets/Payer Movement/PlayerControl.cs
+++ Assets/Payer Movement/PlayerControl.cs
@@ -1,94 +1,11 @@
-
-
-/*
-public class PlayerControl : MonoBehaviour
-{
-
-    public float moveSpeed;
-
-    private bool isMoveing;
-
-    private Animator animator;
-
-    private Vector2 input;
-
-    public LayerMask solidObjectsLayer;
-
-    private void Awake()
-    {
-        animator = GetComponent<Animator>();
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (!isMoveing)
-        {
-            input.x = Input.GetAxisRaw("Horizontal"); //input left/right keyboard or joystick
-            input.y = Input.GetAxisRaw("Vertical");  // input up/down keyboard or joystick
-
-            if (input != Vector2.zero) //if the player pushed a direction key
-            {
-                //setting the direction of the player character animation
-                animator.SetFloat("moveX", input.x);
-                animator.SetFloat("moveY", input.y);
-
-                //no diagnal movement
-                if (input.x != 0) input.y = 0;
-
-                // calculates new target position
-                var targetPos = transform.position;
-                targetPos.x += input.x;
-                targetPos.y += input.y;
-
-                //seeing if the target position can be stepped on (not a wall)
-                if (isWalkable(targetPos))
-                    StartCoroutine(Move(targetPos));
-            }
-        }
-        //updates animator to show moving animation
-        animator.SetBool("isMoving", isMoveing);
-    }
-    IEnumerator Move(Vector3 targetPos)
-    {
-        isMoveing = true; //block new imputs durring movement
-
-        //keeps moving until characher gets to new position
-        while ((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon)
-        {
-            // moves character to new position
-            transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
-            yield return null;
-        }
-        transform.position = targetPos;
-
-        isMoveing = false; // done moving
-    }
-
-    private bool isWalkable(Vector3 targetPos)
-    {
-        //checking if the tile in front of player has an object on the solid object layer. meaning there is a wall there.
-        if (Physics2D.OverlapCircle(targetPos, 0.2f, solidObjectsLayer) != null)
-        {
-            return false;
-        }
-        return true;
-    }
-}
-*/
-
-
-using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
+using TMPro;
 
 public class PlayerControl : MonoBehaviour
 {
+    public int score = 0;
     private bool canJump;
     public float moveSpeed = 5f;       // Horizontal movement speed
     public float jumpForce = 3f;       // Jump power
@@ -96,7 +13,12 @@ public class PlayerControl : MonoBehaviour
     private Rigidbody2D rb;
     private Animator animator;
 
+    [SerializeField] private TextMeshProUGUI scoreText;
     [SerializeField] private float jumpCutMultiplier = 2f;
+    [SerializeField] private List<GameObject> hearts;
+    [SerializeField] private GameObject RestartButton;
+    [SerializeField] private GameObject YouDiedText;
+    [SerializeField] private GameObject YouWonText;
 
     [Header("Ground Check")]
     public Transform groundCheck;      // Empty object under player’s feet
@@ -107,6 +29,10 @@ public class PlayerControl : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        
+        RestartButton.SetActive(false);
+        YouDiedText.SetActive(false);
+        YouWonText.SetActive(false);
     }
 
     void Update()
@@ -116,10 +42,10 @@ public class PlayerControl : MonoBehaviour
         rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
 
         // Jump (only if grounded)
-        Debug.Log(canJump);
+        
         if (Input.GetButtonDown("Jump") && canJump)
         {
-            Debug.Log("Jumped");
+            
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
         // Variable jump↓
@@ -151,19 +77,66 @@ public class PlayerControl : MonoBehaviour
     }
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == ("Ground"))
+        // I used a switch to eliminate if statements
+        switch (collision.gameObject.tag)
         {
-            Debug.Log("on ground");
-            canJump = true;
+            case "Ground":
+                canJump = true;
+                break;
+            case "Hazard":
+                RemoveHeart();
+                break;
+            case "Finish":
+                YouWonText.SetActive(true);
+                RestartButton.SetActive(true);
+                this.enabled = false;
+                break;
         }
     }
+    void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.CompareTag("Coin"))
+            {
+                score += 10;
+                UpdateScoreUI();
+                Destroy(other.gameObject);
+            }
+        }
     void OnCollisionExit2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Ground")
         {
-            Debug.Log("left ground");
             canJump = false;
         }
+    }
+    public void RemoveHeart()
+    {
+        if (hearts.Count > 0)
+        {
+            GameObject lastHeart = hearts[hearts.Count - 1];
+            lastHeart.SetActive(false);   // hides the heart
+            hearts.RemoveAt(hearts.Count - 1);
+
+            Debug.Log("Hearts left: " + hearts.Count);
+
+            if (hearts.Count == 0)
+            {
+                Debug.Log("Game Over!");
+
+                this.enabled = false; // disables this script (no movement/damage)
+
+                YouDiedText.SetActive(true);
+                RestartButton.SetActive(true);
+            }
+        }
+    }
+    private void UpdateScoreUI()
+    {
+        scoreText.text = "Score: +" + score;
+    }
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
 
